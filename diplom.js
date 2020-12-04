f10d5ba54a92365fbb2b5fe03ebafa5e57ac2326
Aleksandr Lazarev <aleqqq132@gmail.com>
2020-12-05 00:24:24 +0300

    diplom v3

--- diplom.js
+++ diplom.js
@@ -6,7 +6,7 @@ login.addEventListener('click' , loginclick);
 
 function loginclick() {
     document.getElementsByTagName("ul")[0].style.transition = '0s';
-    okno.classList.toggle('text-hidden')
+    okno.classList.toggle('text-hidden');
     
 }
 
@@ -15,5 +15,23 @@ let esc = document.querySelector('.exist');
 esc.addEventListener('click' , escClick);
 
 function escClick() {
-    okno.classList.add('text-hidden')
+    okno.classList.add('text-hidden');
+}
+
+let registr = document.querySelector('.poleregistra');
+let okno1 = document.querySelector('.vhod1');
+
+
+registr.addEventListener('click' , regClick);
+
+function regClick() {
+    okno1.classList.toggle('text-hidden1');
+}
+
+let esc1 = document.querySelector('.exist1')
+
+esc1.addEventListener('click' , reg1Click);
+
+function reg1Click() {
+okno1.classList.add('text-hidden1');
 }
