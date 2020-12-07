36a818bdf9598ed5097d0e8ed49152b0c6df3b0d
Aleksandr Lazarev <aleqqq132@gmail.com>
2020-12-07 17:26:32 +0300

    Diplom v3.10.8

--- diplom.js
+++ diplom.js
@@ -35,13 +35,78 @@ esc1.addEventListener('click' , reg1Click);
 function reg1Click() {
 okno1.classList.add('text-hidden1');
 }
-function switcher() {
-if (okno.classList('text-hidden') = true) {
-    okno1.classList.add('text-hidden1') = false;
+// смена экранов!
+// function switcher() {
+// if (okno.classList('text-hidden') = true) {
+//     okno1.classList.add('text-hidden1') = false;
 
-}else if (okno.classList('text-hidden') = false ) {
-    okno1.classList.remove('text-hidden1') = true;
+// }else if (okno.classList('text-hidden') = false ) {
+//     okno1.classList.remove('text-hidden1') = true;
+// }
+// }
+
+// switcher();
+//-------------
+
+let secreg = document.querySelector('.knop');
+let win1 = document.querySelector('.vhod1');
+let up = document.querySelector('.body');
+secreg.addEventListener('click' , secclick);
+
+function secclick() {
+    win1.classList.toggle('text-hidden1');
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
+const getTop = () => window.scrollY;
+let strela = document.querySelector('.arrow');
+let hide = document.querySelector('.arrow-hidden');
+let check = 300;
+
+const updateDashoffset = () => {};
+window.addEventListener('scroll', () => {
+    updateDashoffset();
+
+if (getTop() > check) {
+    hide.classList.remove('arrow-hidden')
+}else {
+    hide.classList.add('arrow-hidden')
+}
+});
+
+strela.addEventListener('click' , strelaClick);
+
+function strelaClick() {
+window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});    
 }
 
-switcher();
+
+// const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+// let strela = document.querySelector('.arrow');
+// let hide = document.querySelector('.arrow-hidden');
+// let check = 300;
+
+// const updateDashoffset = () => {};
+// window.addEventListener('scroll', () => {
+//     updateDashoffset();
+
+// if (getTop() > check) {
+//     hide.classList.remove('arrow-hidden')
+// }else {
+//     hide.classList.add('arrow-hidden')
+// }
+// });
+
+// strela.addEventListener('click' , strelaClick);
+
+// function strelaClick() {
+// window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+// });    
+// }
